1987d555b7c1d123591bebc8ed25a7a500ccaf55
nikozka15 <vshyrshova@gmail.com>
2023-08-15 08:30:18 +0100

    #7 - Format files

--- src/main/java/com/nikozka/services/ItemService.java
+++ src/main/java/com/nikozka/services/ItemService.java
@@ -2,11 +2,9 @@ package com.nikozka.services;
 
 import com.nikozka.dao.ItemRepository;
 import com.nikozka.dtos.Item;
-
+import com.nikozka.dtos.ListedItem;
 import java.util.List;
 import java.util.UUID;
-
-import com.nikozka.dtos.ListedItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -37,8 +35,9 @@ public class ItemService {
 
   public List<ListedItem> getListedItems() {
     return itemRepository.findAll().stream()
-            .filter(com.nikozka.dao.entities.Item::getListed)
-            .map(ListedItem::convert).toList();
+        .filter(com.nikozka.dao.entities.Item::getListed)
+        .map(ListedItem::convert)
+        .toList();
   }
 
   public Item findItemById(UUID itemId) {
