91b798a93fd009de4f46f761ca406909a6da64a3
nikozka15 <127139578+nikozka15@users.noreply.github.com>
2023-08-15 08:28:11 +0100

    #7 - Format files (#10)

--- src/main/java/com/nikozka/dtos/ListedItem.java
+++ src/main/java/com/nikozka/dtos/ListedItem.java
@@ -3,7 +3,8 @@ package com.nikozka.dtos;
 import java.util.UUID;
 
 public record ListedItem(UUID id, Item item) {
-    public static ListedItem convert(com.nikozka.dao.entities.Item item) {
-        return new ListedItem(item.getId(), new Item(item.getTitle(), item.getDescription(), item.getCity()));
-    }
+  public static ListedItem convert(com.nikozka.dao.entities.Item item) {
+    return new ListedItem(
+        item.getId(), new Item(item.getTitle(), item.getDescription(), item.getCity()));
+  }
 }
